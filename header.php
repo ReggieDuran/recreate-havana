e6c4cb4b82322cf8fc69e4f2452fe68125dafab6
Reggie <reggieduran15@gmail.com>
2021-05-06 14:21:43 +0800

    Added header, footer & functions file

---
+++ header.php
@@ -0,0 +1,39 @@
+<?php 
+/**
+ * The header for our theme.
+ * 
+ * Displays all of the <head> section and everything up till <div id="conten">
+ * 
+ * @package havana
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<?php wp_head(); ?>
+<?php 
+
+$bodyClasses = '';
+
+/**
+ * Get the header image to display
+ */
+$headerImageUrl = '';
+$headerImageGradientColor = '';
+$stop1Opacity = 0.6;
+$stop2Opactiy = 0.4;
+if ( get_header_image() ) {
+    $headerImageUrl = get_header_image();
+    $headerImageGradientColor = '41,51,56';
+}
+?>
+</head>
+
+
+<body <?php body_class( $bodyClasses ) ?>>
+    <?php var_dump($headerImageUrl);?>
+</body>
